f05109bb26ca36df00030f258dfb7d7ebffeabb9
Rodrigo Nogueira <101781873+RodrigoSNogueira@users.noreply.github.com>
2024-09-04 18:19:27 -0300

    Time: 38 ms (37.17%), Space: 16.6 MB (15.83%) - LeetHub

--- 0050-powx-n/0050-powx-n.py
+++ 0050-powx-n/0050-powx-n.py
@@ -1,6 +1,22 @@
 class Solution:
     def myPow(self, x: float, n: int) -> float:
         
-        powerof = pow(x, n)
+        #if the exponent is 0, the result is 1
+        if n == 0:
+            return 1
 
-        return powerof
+        #if the exponent is negative, the base is inverted (1/x)
+        if n < 0:
+            x = 1 / x
+            n = -n
+        
+        #the exponent is even
+        if n % 2 == 0:
+            half = self.myPow(x, n // 2)
+            return half * half
+
+        #the exponent is negative
+        else:
+            return x * self.myPow(x, n-1)
+        
+    
